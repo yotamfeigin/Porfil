d7590675c8ac7d874f85a06f648d0b2722f5d104
yotamfeigin <yotam181999@gmail.com>
2024-06-23 14:18:42 +0300

    .

--- components/Common/Intro/Contact.jsx
+++ components/Common/Intro/Contact.jsx
@@ -7,10 +7,9 @@ const Contact = () => {
     <div className='flex flex-col space-y-2 pt-6'>
       <div className='flex flex-col'>
         <span className='text-Snow text-xs font-bold'>Email Address</span>
-
-        <span className='text-xs text-gray-600' mailto>
-          <Link href={`mailto:${CONTACTS.EMAIL}`}>{CONTACTS.EMAIL}</Link>
-        </span>
+        <Link href={`mailto:${CONTACTS.EMAIL}`}>
+          <span className='text-xs text-gray-600'>{CONTACTS.EMAIL}</span>
+        </Link>
       </div>
       <div className='flex flex-col'>
         <span className='text-Snow text-xs font-bold'>Phone</span>
--- components/HomeComponents/Banner.jsx
+++ components/HomeComponents/Banner.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import Typewriter from 'typewriter-effect';
 import BannerLayout from '../Common/BannerLayout';
 import Image from 'next/image'; // Import Image from next/image for optimized images
@@ -6,14 +6,12 @@ import Image from 'next/image'; // Import Image from next/image for optimized im
 const Banner = () => {
     const typewriterRef = useRef(null);
 
-    useEffect(() => {
-        // Pause typewriter effect when component unmounts
-        return () => {
-            if (typewriterRef.current) {
-                typewriterRef.current.stop();
-            }
-        };
-    }, []);
+    const roles = [
+        'nearly graduated CS student',
+        'Unity Developer',
+        'Full Stack Developer',
+        'Junior Programmer',
+    ];
 
     return (
         <BannerLayout>
@@ -21,34 +19,34 @@ const Banner = () => {
                 <div className="bg-LightGray/10 w-[95%] h-[90%] px-4 py-2 rounded-xl overflow-hidden flex md:block">
                     <div className="flex items-center md:items-center md:justify-around">
                         <div>
-                            <h1 className="text-3xl sm:text-4xl xl:text-5xl text-Snow font-bold">Hello, Check This Out!</h1>
+                            <h1 className="text-3xl sm:text-4xl xl:text-5xl text-Snow font-bold">Welcome to my portfolio!</h1>
                             <div className="py-4 font-cascadia-normal text-Snow pb-4 text-xs h-20 lg:h-auto">
-                                <span>{"<"}<span className="text-Green sm:text-base xl:text-lg font-bold">div</span>
-                                {">"}{' '}
+                                <span>
+                                    {"<"}
+                                    <span className="text-Green sm:text-base xl:text-lg font-bold">div</span>
+                                    {">"}{' '}
                                     <span className="text-Snow sm:text-xl xl:text-2xl font-bold">
-                                        I am a{' '}
+                                        I am {' '}
                                         <span className="inline-block" ref={typewriterRef}>
                                             <Typewriter
                                                 options={{
-                                                    strings: [
-                                                        'Nearly graduated Computer Science student',
-                                                        'Unity Developer',
-                                                        'Full Stack Developer',
-                                                        'General Programmer',
-                                                    ],
+                                                    strings: roles.map(role =>
+                                                        `a ${role}`
+                                                    ),
                                                     autoStart: true,
                                                     loop: true,
                                                 }}
                                             />
                                         </span>
                                     </span>{' '}
-                                    {"</"}<span className="text-Green sm:text-base xl:text-lg font-bold">div</span>
-                                {">"}{' '}
+                                    {"</"}
+                                    <span className="text-Green sm:text-base xl:text-lg font-bold">div</span>
+                                    {">"}{' '}
                                 </span>
                             </div>
                         </div>
                         <div className="w-48 h-52 relative hidden md:block">
-                            <Image src="/images/Wave.gif" alt="emoji" layout="fill" objectFit="cover" />
+                            <Image src="/images/Wave.gif" alt="emoji" fill style={{ objectFit: 'cover' }} />
                         </div>
                     </div>
                 </div>
