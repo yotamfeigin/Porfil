a4eaea85b1128653c4dc1f8874d4cd76426d8459
yotamfeigin <yotam181999@gmail.com>
2024-06-29 17:02:03 +0300

    .

--- components/HomeComponents/Expertise/ExpertiseCard.jsx
+++ components/HomeComponents/Expertise/ExpertiseCard.jsx
@@ -4,8 +4,8 @@ const ExpertiseCard = ({ data }) => {
     return (
         <CardLayout>
             <div className="h-full space-y-2 p-8 card_stylings">
-                <div className=" text-Snow">{data.title}</div>
-                <div className="text-sm text-LightGray font-normal">
+                <div className="text-Snow" style={{ fontSize: '1.5rem' }}>{data.title}</div>
+                <div className="text-sm text-LightGray font-normal" style={{ fontSize: '1.25rem' }}>
                     {data.desc}
                 </div>
             </div>
--- pages/api/expertise.js
+++ pages/api/expertise.js
@@ -2,28 +2,25 @@ const expertise = [
     {
         id: 0,
         title: 'Unity Development',
-        desc: "I am a self-taught Unity developer with hands-on experience in creating games. Through building my own game, I gained extensive knowledge in game design, physics, and scripting within Unity. Additionally, I worked on a project as a freelancer, which further honed my skills in developing interactive and engaging game elements. My journey with Unity has been both educational and rewarding, equipping me with the skills to tackle various game development challenges.",
+        desc: "As a self-taught Unity developer, I've focused on understanding game design, physics, and scripting elements to streamline development and enhance performance. This approach has made my work more efficient and effective."
     },
     {
         id: 1,
         title: 'Full Stack Development',
-        desc: 'During my degree, I have hands-on experience with full stack development, particularly with the MERN stack. I have worked on projects like creating a functional chat application using Node.js as the server and React for the front end with HTML/CSS. Additionally, my final project for my degree involves building an application using React and TypeScript. These experiences have equipped me with a solid understanding of server-side rendering, API integration, and database management.',
+        desc: 'Experienced with the MERN stack, including projects like a chat application using Node.js and React. Final degree project involves React and TypeScript, focusing on server-side rendering, API integration, and database management.'
     },
-
     {
         id: 2,
         title: 'Mathematical Thinking',
-        desc: 'With a foundation in Mathematics and Statistics from my studies at Tel Aviv University and strongly reinforced by my degree at Bar-Ilan University, I have developed excellent analytical and problem-solving skills. My grades in the mathematics courses at both institutions have been excellent. I have always had a mathematical and analytical mind, which is crucial for algorithm design, data analysis, and tackling complex programming challenges, ensuring efficient and effective solutions in my projects.',
+        desc: 'Strong foundation in Mathematics and Statistics, emphasizing analytical and problem-solving skills crucial for algorithm design, data analysis, and complex programming challenges.'
     },
-    
     {
         id: 3,
         title: 'GitHub and Jira',
-        desc: "I have basic experience with GitHub and Jira, using them for version control and project management in my academic projects. GitHub's pull requests and issue tracking have helped streamline collaboration, while Jira has been useful for task assignment and tracking progress. Though on a smaller scale, these tools have been essential in keeping projects organized and on track.",
-    },
-  
-    
-]
+        desc: "Basic experience using GitHub for version control (pull requests, issue tracking) and Jira for project management (task assignment, progress tracking) in academic projects."
+    }
+];
+
 
 export default function handler(req, res) {
     res.status(200).json(expertise)
